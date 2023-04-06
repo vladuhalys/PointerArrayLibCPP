565c9a0146cad53f6410e5c317d1afd230f137ba
vladuhalys <vlad.lysenko1999@gmail.com>
2023-04-06 17:41:41 +0300

    Add remove functions

--- application.cpp
+++ application.cpp
@@ -1,7 +1,5 @@
 #include "pointer_array_lib.h"
 
-
-
 int main()
 {
 	int size = 10;
@@ -15,11 +13,31 @@ int main()
 		cout << "Enter value: ";
 		int value;
 		cin >> value;
-		cout << "Enter index: ";
-		int index;
-		cin >> index;
-		insert<int>(array, size, value, index);
-		cout << "----------after push item-----------------" << endl;
+		cout << "Menu: 1 - push, 2 - remove" << endl;
+		int choose;
+		cin >> choose;
+		switch (choose)
+		{
+		case 1:
+		{
+			cout << "Enter index: ";
+			int index;
+			cin >> index;
+			insert<int>(array, size, value, index);
+		}break;
+		case 2:
+		{
+			if (remove_at<int>(array, size, value)) {
+				cout << "Item was remove" << endl;
+			}
+			else
+			{
+				error_message("Item not found" << endl);
+			}
+			
+		}break;
+		}
+		cout << "----------after change-----------------" << endl;
 		cout << "size : " << size << endl;
 		print<int>(array, size);
 		system("pause");
